e96daebb9a439e08ff93af153a508306656580d1
Craig Ingram <cingram@us.ibm.com>
2022-04-05 11:14:30 -0400

    Added OSS badge (#58)
    * Added OSS badge
    
    * Updating linter
    
    * Upgraded dependency versions
    
    * Updated go version used used in Dockerfiles
    
    * Updated GHA for setup-go and checkout
    
    * Explicitly added GO_VERSION to setup-go
    Reordered checkout and setup-go
    
    * Increased time for a backend response in test

--- tests/ready_pods_test.go
+++ tests/ready_pods_test.go
@@ -125,7 +125,7 @@ var _ = Describe("Varnish cluster", func() {
 				return 0, err
 			}
 			return resp.StatusCode, nil
-		}, time.Second*20, time.Second*2).Should(Equal(200))
+		}, time.Second*30, time.Second*2).Should(Equal(200))
 		Expect(resp.Header.Get("X-Varnish-Cache")).To(Equal("MISS"))
 		body, err := ioutil.ReadAll(resp.Body)
 		Expect(err).NotTo(HaveOccurred())

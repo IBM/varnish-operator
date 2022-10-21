b372ce9c2c6245abc6247ad1306d46fa21cc3ce3
Craig Ingram <cingram@us.ibm.com>
2022-10-21 15:39:43 -0400

    Added logic to handle podman buildx behavior differences   `podman buildx` auto-loads the image into the local docker repo   `podman buildx` doesn't support `--push` arg   `docker pull` is not necessary with podman Refactored create_dev_cluster.sh
    Signed-off-by: Craig Ingram <cingram@us.ibm.com>

--- hack/create_dev_cluster.sh
+++ hack/create_dev_cluster.sh
@@ -8,7 +8,10 @@ fi
 
 varnish_namespace="varnish-operator"
 cluster_name="e2e-tests"
-container_image="ibmcom/varnish-operator:local"
+repo="ibmcom"
+container_image="$repo/varnish-operator:local"
+build_args="--build-arg VERSION=local --platform linux/arm64,linux/amd64 --push"
+podman_in_use=false
 
 if ! which docker; then
     echo -e "Install docker first"
@@ -25,15 +28,17 @@ if ! which helm >/dev/null; then
     exit 1
 fi
 
-#if which podman >/dev/null; then
-#  img=$(podman images | grep 'ibmcom/varnish-operator' | grep local | cut -f1 -d' ')
-#  if [ -n "$img" ]; then
-#    echo "using podman image: $img"
-#    container_image=$img
-#  else
-#    echo "podman installed but varnish-operator image not found. using default container_image"
-#  fi
-#fi
+if which podman >/dev/null; then
+  img=$(podman images | grep '$repo/varnish-operator' | grep local | cut -f1 -d' ')
+  if [ -n "$img" ]; then
+    podman_in_use=true
+    echo "using podman image: $img"
+    # podman doesn't support the `--push` option. hopefully no other options start with `--push`...
+    build_args=$(echo ${build_args/--push})
+  else
+    echo "podman installed but varnish-operator image not found"
+  fi
+fi
 
 kind delete cluster --name $cluster_name > /dev/null 2>&1
 kind create cluster --name $cluster_name --image kindest/node:v$kube_version --kubeconfig ./e2e-tests-kubeconfig
@@ -42,19 +47,21 @@ export KUBECONFIG=./e2e-tests-kubeconfig
 
 kubectl create ns $varnish_namespace
 
-docker buildx build --build-arg VERSION=local --platform linux/arm64,linux/amd64 --push -f Dockerfile -t ibmcom/varnish-operator:local .
-docker buildx build --build-arg VERSION=local --platform linux/arm64,linux/amd64 --push -f Dockerfile.varnishd -t ibmcom/varnish:local .
-docker buildx build --build-arg VERSION=local --platform linux/arm64,linux/amd64 --push -f Dockerfile.controller -t ibmcom/varnish-controller:local .
-docker buildx build --build-arg VERSION=local --platform linux/arm64,linux/amd64 --push -f Dockerfile.exporter -t ibmcom/varnish-metrics-exporter:local .
+docker buildx build $build_args -f Dockerfile -t $container_image .
+docker buildx build $build_args -f Dockerfile.varnishd -t $repo/varnish:local .
+docker buildx build $build_args -f Dockerfile.controller -t $repo/varnish-controller:local .
+docker buildx build $build_args -f Dockerfile.exporter -t $repo/varnish-metrics-exporter:local .
 
-docker pull ibmcom/varnish-operator:local
-docker pull ibmcom/varnish:local
-docker pull ibmcom/varnish-controller:local
-docker pull ibmcom/varnish-metrics-exporter:local
+if [ "$podman_in_use" = false ]; then
+  docker pull $container_image
+  docker pull $repo/varnish:local
+  docker pull $repo/varnish-controller:local
+  docker pull $repo/varnish-metrics-exporter:local
+fi
 
-kind load docker-image -n $cluster_name ibmcom/varnish-operator:local
-kind load docker-image -n $cluster_name ibmcom/varnish:local
-kind load docker-image -n $cluster_name ibmcom/varnish-controller:local
-kind load docker-image -n $cluster_name ibmcom/varnish-metrics-exporter:local
+kind load docker-image -n $cluster_name $container_image
+kind load docker-image -n $cluster_name $repo/varnish:local
+kind load docker-image -n $cluster_name $repo/varnish-controller:local
+kind load docker-image -n $cluster_name $repo/varnish-metrics-exporter:local
 
 helm install vo varnish-operator --namespace=$varnish_namespace --wait --set container.imagePullPolicy=Never --set container.image=$container_image
